36a79b08e3da273555ca681aac85194aa3a8ffca
Matias Asenjo Pino <matia.cba74@gmail.com>
2022-07-13 12:34:29 -0300

    remove / from forgot controller

--- users/usersController.js
+++ users/usersController.js
@@ -88,7 +88,7 @@ const forgot = async(req,res,next)=>{
         email:dbResponse[0].email
     }
     const token = await tokenSign(user,`15m`)
-    const link = `${public_url}/users/reset/${token}`
+    const link = `${public_url}users/reset/${token}`
     let mailDetails = {
         from:"sudBank@gmail.com",
         to:user.email,
